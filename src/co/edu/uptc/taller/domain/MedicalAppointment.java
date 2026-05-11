7c94a00ddfc092886325edbcb995fb1f83b1939d
Narwill Daniel Zabala Mata <danielzabala707@gmail.com>
2026-05-11 12:17:49 -0500

    se crean las clases de cita medica, repositorio de doctor y se modifica la clase paciente

---
+++ src/co/edu/uptc/taller/domain/MedicalAppointment.java
@@ -0,0 +1,68 @@
+package co.edu.uptc.taller.domain;
+
+import java.time.LocalDateTime;
+
+import co.edu.uptc.taller.enums.Priority;
+
+/**
+ * Entidad que representa una cita médica
+ * @author TuNombre
+ */
+public class MedicalAppointment {
+    private long idMedicalAppointment;
+    private LocalDateTime timeAppointment;
+    private Patient patient;
+    private Doctor doctor;
+    
+    public MedicalAppointment(long idMedicalAppointment, LocalDateTime timeAppointment, 
+                              Patient patient, Doctor doctor) {
+        this.idMedicalAppointment = idMedicalAppointment;
+        this.timeAppointment = timeAppointment;
+        this.patient = patient;
+        this.doctor = doctor;
+    }
+
+	public long getIdMedicalAppointment() {
+		return idMedicalAppointment;
+	}
+
+	public void setIdMedicalAppointment(long idMedicalAppointment) {
+		this.idMedicalAppointment = idMedicalAppointment;
+	}
+
+	public LocalDateTime getTimeAppointment() {
+		return timeAppointment;
+	}
+
+	public void setTimeAppointment(LocalDateTime timeAppointment) {
+		this.timeAppointment = timeAppointment;
+	}
+
+	public Patient getPatient() {
+		return patient;
+	}
+
+	public void setPatient(Patient patient) {
+		this.patient = patient;
+	}
+
+	public Doctor getDoctor() {
+		return doctor;
+	}
+
+	public void setDoctor(Doctor doctor) {
+		this.doctor = doctor;
+	}
+    
+	public Priority getPriority() {
+        return patient != null ? patient.getPriority() : Priority.LOW;
+    }
+	
+	@Override
+    public String toString() {
+        return String.format("Cita %d: %s - Dr. %s (Prioridad: %s)", 
+                idMedicalAppointment, timeAppointment.toString(), 
+                doctor.getFullName(), getPriority());
+    }
+    
+}
--- src/co/edu/uptc/taller/repository/PatientRepository.java
+++ src/co/edu/uptc/taller/repository/PatientRepository.java
@@ -1,5 +1,38 @@
 package co.edu.uptc.taller.repository;
 
-public class PatientRepository {
 
+import java.util.*;
+import co.edu.uptc.taller.domain.Patient;
+import co.edu.uptc.taller.enums.IdentificationType;
+
+/**
+ * Repositorio para gestionar pacientes garantizando unicidad
+ * @author Narwill Daniel Zabala Mata
+ */
+
+public class PatientRepository {
+    private Map<String, Patient> patientsById;      // Clave: tipoID:numeroID
+    private Map<String, Patient> patientsByEmail;   // Clave: email
+    
+    public PatientRepository() {
+        this.patientsById = new HashMap<>();
+        this.patientsByEmail = new HashMap<>();
+    }
+    
+    private String generateIdKey(IdentificationType type, long id) {
+        return type.name() + ":" + id;
+    }
+    
+    public Patient findByIdentification(IdentificationType type, long id) {
+        return patientsById.get(generateIdKey(type, id));
+    }
+    
+    public Patient findByEmail(String email) {
+        return patientsByEmail.get(email);
+    }
+    
+    public List<Patient> getAllPatients() {
+        return new ArrayList<>(patientsById.values());
+    }
+    
 }
